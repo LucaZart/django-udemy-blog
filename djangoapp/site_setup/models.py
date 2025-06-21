e5247dbca8aa5fc6795cc67957652260e9251183
LucaZart <lucaszart18@gmail.com>
2025-06-21 11:34:24 -0300

    Adicionando o site_setup no context_processors e criando link entre os links e o site setup

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,21 +1,30 @@
 from django.db import models
 
+
 class MenuLink(models.Model):
     class Meta:
-        verbose_name = 'Menu Link'
-        verbose_name_plural = 'Menu Links'
+        verbose_name = "Menu Link"
+        verbose_name_plural = "Menu Links"
 
     text = models.CharField(max_length=50)
     url_or_path = models.CharField(max_length=2048)
     new_tab = models.BooleanField(default=False)
+    site_setup = models.ForeignKey(
+        "SiteSetup",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        default=None,
+    )
 
     def __str__(self) -> str:
         return self.text
-    
+
+
 class SiteSetup(models.Model):
     class Meta:
-        verbose_name = 'Setup'
-        verbose_name_plural = 'Setup'
+        verbose_name = "Setup"
+        verbose_name_plural = "Setup"
 
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=255, default="")
